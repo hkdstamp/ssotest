4b493bf59febdcf1f0ab80b8dad16d70deff1cd7
Haruhiko Kodo <kodo.haruhiko@mobingi.com>
2017-12-08 17:41:41 +0900

    Google verify

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 error_reporting(0);
 phpinfo();
+</?php>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="google-site-verification" content="HYpeXXGI1lNOhc5FIebGRetpP-nxjE7wqYH-6l4Wle4" />
+</head>
+
+<body>
+testtest
+</body>
+</html>
